a719cb47195eba314dee25bdab3ea5306103a039
Victor Prado <prado.victor1005@gmail.com>
2024-03-02 13:42:06 -0300

    first commit

---
+++ src/app/components/Sidebar/MainNavigation/index.tsx
@@ -0,0 +1,33 @@
+import { BarChart, CheckSquare, ChevronDown, Flag, Home, SquareStack, Users } from "lucide-react";
+import { NavItem } from "./NavItem";
+
+export function MainNavigation() {
+  return (
+    <nav className="space-y-0.5">
+      <NavItem
+        title="Home"
+        icon={Home}
+      />
+      <NavItem
+        title="Dashboard"
+        icon={BarChart}
+      />
+      <NavItem
+        title="Projects"
+        icon={SquareStack}
+      />
+      <NavItem
+        title="Tasks"
+        icon={CheckSquare}
+      />
+      <NavItem
+        title="Reporting"
+        icon={Flag}
+      />
+      <NavItem
+        title="Users"
+        icon={Users}
+      />
+    </nav>
+  )
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr'
+      },
+      colors: {
+        diego: '#8257e6',
+      }
+    },
+  },
+  plugins: [],
+};
+export default config;
